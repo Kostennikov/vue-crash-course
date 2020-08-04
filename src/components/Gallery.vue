41e07cc0a4776907aef19e09374ac383493aab18
Kostennikov <pavel.kostennikov@gmail.com>
2020-08-04 18:40:13 +0300

    init commit

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,24 +1,54 @@
 <template>
   <div>
-    <li id="new-data" ></li>
+    {{ files }}
+    <h2>Last optimized</h2>
+    <div v-for="file in filesReversed" :key="file.id">
+      <h1 v-if="progress === 100">Completed</h1>
+      <h1 v-else>Not Completed</h1>
+      <img
+              class="image"
+              :src="file.base64 || file.link"
+
+
+      />
+      <div class="progress" :style="'width: '+progress+'%'" v-if="showProgress"></div>
+      <!-- <ProgressBar />
+     -->
+<!--      <p>{{file.name}}</p>-->
+    </div>
    
   </div>
 </template>
 <script>
-// export default{
-    
-//     data() {
-//     return {
-//       files: [
-//         { id: 1, binaryFile: null, domElement: null, link: "", base64: "" },
-//         { id: 2, binaryFile: null, domElement: null, link: "", base64: "" },
-//         { id: 3, binaryFile: null, domElement: null, link: "", base64: "" }
-//       ]
-//     };
-//   },
-// }
+export default{
+  props:
+    ['files', "progress"],
+
+
+  data() {
+    return {
+
+    };
+  },
+  computed: {
+    filesReversed: function() {
+      return this.files.slice().reverse();
+    }
+  },
+  methods: {
+
+  },
+}
 </script>
 <style scoped>
-
-
+  .image {
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+    object-fit: cover;
+  }
+  .progress {
+    background-color: #0f0;
+    border: 1px solid red;
+  }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -13,27 +13,16 @@
 
         <button id="send" @click="sendFiles">send</button>
         <div id="output">
-          <h2>Last optimized</h2>
-          <div v-for="file in filesReversed" :key="file.id">
-            <h1 v-if="progress === 100">Completed</h1>
-            <h1 v-else>Not Completed</h1>
-            <img
-              :src="file.base64 || file.link"
-              alt
-              :class="['image', {'image_ok':file.status=='ok','image_bad':file.status=='bad'}]"
-            />
-            <div class="progress" :style="'width: '+progress+'%'" v-if="showProgress"></div>
-            <!-- <ProgressBar />
-           -->
-            <p>{{file.name}}</p>
-          </div>
+
         </div>
       </div>
     </section>
 
     <div class="gallery">
       <ul>
-        <Gallery />
+        <Gallery
+        :files="files"
+        />
       </ul>
     </div>
   </div>
@@ -60,46 +49,40 @@ export default {
   props: ["progress"],
   data() {
     return {
-      files: [
-        // {
-        //   link: "http://deareditor.ru/rvk/images/to-numbers.png",
-        //   name: "fghjk"
-        // }
-      ]
+      files: [],
+      info: {
+        base64: 'base64'
+      }
     };
   },
-  computed: {
-    filesReversed: function() {
-      return this.files.slice().reverse();
-    }
-  },
+
   methods: {
     
     fileChange: function(event) {
-      var input = event.target;
-      for (var i = 0, l = input.files.length; i < l; i++) {
+      let input = event.target;
+      for (let i = 0, l = input.files.length; i < l; i++) {
         this.prepareFile(input.files[i]);
       }
     },
     prepareFile: function(file) {
-      var reader = new FileReader(),
+      let reader = new FileReader(),
         info = { binaryFile: file, name: file.name };
       this.files.push(info);
 
       reader.addEventListener("load", function(event) {
-        var base64 = event.target.result;
+        let base64 = event.target.result;
         Vue.set(info, "base64", base64);
       });
       reader.readAsDataURL(file);
     },
     // sendFiles: function() {
-    //   for (var i = 0, l = this.files.length; i < l; i++) {
+    //   for (let i = 0, l = this.files.length; i < l; i++) {
     //     Vue.set(this.files[i], "status","ok");
     //   }
     // },
 
     sendFiles: function() {
-      // for (var i = 0, l = this.files.length; i < l; i++) {
+      // for (let i = 0, l = this.files.length; i < l; i++) {
       //   Vue.set(this.files[i], "status", "progress");
       // }
 
